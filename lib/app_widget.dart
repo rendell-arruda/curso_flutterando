8887e321337d73a1b073fe05b14d05711846f4a4
Rendell Arruda <rendellarruda3@gmail.com>
2023-01-31 10:55:00 -0300

    add scaffold

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -9,7 +9,7 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.red),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: HomePage(),
     );
   }
