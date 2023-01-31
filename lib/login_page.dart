9cf43e49b0c3fda8e7fb0b07031f26926336f83c
Rendell Arruda <rendellarruda3@gmail.com>
2023-01-31 20:39:32 -0300

    add img background

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -12,71 +12,91 @@ class _LoginPageState extends State<LoginPage> {
   String email = '';
   String password = '';
 
+  Widget _body() {
+    return Column(
+      children: [
+        SingleChildScrollView(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 200,
+                      height: 200,
+                      child: Image.asset('assets/images/logo.png'),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextField(
+                      onChanged: (text) {
+                        email = text;
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      onChanged: (text) {
+                        password = text;
+                      },
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        if (email == 'rendellarruda3@gmail.com' &&
+                            password == '123') {
+                          Navigator.of(context).pushReplacementNamed('/home');
+                        } else {
+                          print('login inválido');
+                        }
+                      },
+                      child: Text(
+                        'Entrar',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    )
+                  ]),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 100,
-                    height: 100,
-                    child: Image.asset('assets/images/logo.png'),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  TextField(
-                    onChanged: (text) {
-                      email = text;
-                    },
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  TextField(
-                    onChanged: (text) {
-                      password = text;
-                    },
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      if (email == 'rendellarruda3@gmail.com' &&
-                          password == '123') {
-                        Navigator.of(context).pushReplacementNamed('/home');
-                      } else {
-                        print('login inválido');
-                      }
-                    },
-                    child: Text(
-                      'Entrar',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  )
-                ]),
-          ),
+        body: Stack(
+      children: [
+        SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset(
+              'assets/images/background.jpg',
+              fit: BoxFit.cover,
+            )),
+        Container(
+          color: Colors.white.withOpacity(0.10),
         ),
-      ),
-    );
+        _body(),
+      ],
+    ));
   }
 }
