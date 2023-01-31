62b77378c5dc3a0b71f66d46e33219b4433961ca
Rendell Arruda <rendellarruda3@gmail.com>
2023-01-31 10:44:05 -0300

    separa widgets em arquivos

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,8 @@
 import 'package:flutter/material.dart';
+import 'app_widget.dart';
 
 main() {
-  runApp(AppWidget(
-    title: 'Flutter2',
-  ));
-}
-
-class AppWidget extends StatelessWidget {
-  final String title;
-
-  const AppWidget({required this.title, Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.red),
-      home:HomePage() ,
-    );
-  }
+  runApp(AppWidget());
 }
 
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text('Flutterando')),
-    );
-  }
-}
